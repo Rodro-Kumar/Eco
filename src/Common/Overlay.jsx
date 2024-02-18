347493b634851f53f870efe8055dc50380ab7e97
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-02-18 19:39:52 +0600

    overlay

--- src/Common/Overlay.jsx
+++ src/Common/Overlay.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Button from "./Button";
 
-const Overlay = ({ className }) => {
+const Overlay = ({ className, DiscooutRightText, Discout, AdsHeading }) => {
   return (
     <>
       <div className={className}>
         <h3 className="text-4xl font-DMsans font-bold text-primaryFontColor pb-9">
-          Phones Sale
+          {AdsHeading ? AdsHeading : "Phones Sale "}
         </h3>
         <p className="text-base font-DMsans font-normal text-[#262626c4] pb-12">
-          Up to{" "}
-          <span className="text-[40px] font-bold text-primaryFontColor">
-            30%
+          Up to
+          <span className="text-[40px] font-bold text-primaryFontColor px-2">
+            {Discout ? Discout : "30%"}
           </span>
-          sale for all phones!
+          {DiscooutRightText ? DiscooutRightText : "sale for all phones!"}
         </p>
         <Button></Button>
       </div>
