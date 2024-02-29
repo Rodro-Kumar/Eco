9c210b687ad33647f7110eec637b4b88b73d8799
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-02-29 12:38:06 +0600

    Special Offer Data

---
+++ SpecialOfferData/SpecialOffer.js
@@ -0,0 +1,45 @@
+import product9 from "../src/assets/product9.png";
+import product10 from "../src/assets/product10.png";
+import product11 from "../src/assets/product11.png";
+import product12 from "../src/assets/product12.png";
+
+const SpecialOfferData = [
+  {
+    id: 1,
+    title: "Basic Crew Neck Tee",
+    price: "$10.00",
+    badge: true,
+    color: true,
+    productImg: product9,
+    productStatus: "New",
+  },
+  {
+    id: 2,
+    title: "Basic Crew Neck Tee",
+    price: "$35.00",
+    badge: true,
+    color: true,
+    productImg: product10,
+    productStatus: "Trending",
+  },
+  {
+    id: 3,
+    title: "Basic Crew Neck Tee",
+    price: "$80.00",
+    badge: true,
+    color: true,
+    productImg: product11,
+    productStatus: "Sold",
+  },
+  {
+    id: 4,
+    title: "Basic Crew Neck Tee",
+    price: "$50.00",
+    badge: true,
+    color: true,
+    productImg: product12,
+    productStatus: "75%",
+  },
+];
+
+export default SpecialOfferData;
