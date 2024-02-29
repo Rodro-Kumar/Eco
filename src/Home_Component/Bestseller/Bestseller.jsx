71d55fb98a512c9954a28b48cb698305953afe7c
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-02-29 11:13:50 +0600

    Bestseller part update

--- src/Home_Component/Bestseller/Bestseller.jsx
+++ src/Home_Component/Bestseller/Bestseller.jsx
@@ -1,12 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../Common/Card";
-import Flex from "../../Common/Flex";
-import product5 from "../../assets/product5.png";
-import product6 from "../../assets/product6.png";
-import product7 from "../../assets/product7.png";
-import product8 from "../../assets/product8.png";
+import Button from "../../Common/Button";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+import Slider from "react-slick";
+import BestSellerData from "../../../BestSellerData/BestsellerData";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "#979797",
+        height: "60px",
+        width: "60px",
+        position: "absolute",
+        top: "40%",
+        transform: "translateY(-40%)",
+        right: "33px",
+        borderRadius: "50%",
+      }}
+      onClick={onClick}
+    >
+      <div className="flex justify-center h-full items-center cursor-pointer hover:bg-[#b3b3b3] rounded-full">
+        <FaLongArrowAltRight className="text-white text-lg" />
+      </div>
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "#979797",
+        height: "60px",
+        width: "60px",
+        position: "absolute",
+        top: "40%",
+        transform: "translateY(-40%)",
+        left: "0",
+        borderRadius: "50%",
+        zIndex: "1",
+      }}
+      onClick={onClick}
+    >
+      <div className="flex justify-center h-full items-center cursor-pointer hover:bg-[#b3b3b3] rounded-full">
+        <FaLongArrowAltLeft className="text-white text-lg" />
+      </div>
+    </div>
+  );
+}
+
+const settings = {
+  dots: false,
+  arrows: true,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  nextArrow: <SampleNextArrow />,
+  prevArrow: <SamplePrevArrow />,
+};
 
 const Bestseller = () => {
+  const [ourBestSellerData, setourBestSellerData] = useState(BestSellerData);
   return (
     <>
       <div>
@@ -15,12 +78,22 @@ const Bestseller = () => {
             <h4 className="text-[40px] font-DMsans font-bold text-primaryFontColor pb-12">
               Our Bestsellers
             </h4>
-            <Flex className={"gap-x-9"}>
-              <Card productImg={product5} />
-              <Card productImg={product6} />
-              <Card productImg={product7} />
-              <Card productImg={product8} />
-            </Flex>
+            <Slider {...settings}>
+              {ourBestSellerData?.map((item) => (
+                <Card
+                  key={item.id}
+                  img={item.productImg}
+                  badge={
+                    item.badge ? (
+                      <Button className={"py-[7px] px-8"}>
+                        {item.productStatus}
+                      </Button>
+                    ) : null
+                  }
+                  colorVariant={item.color}
+                />
+              ))}
+            </Slider>
           </div>
         </div>
       </div>
