707efcb32f6b28a955409f5ecd99910d159d43ec
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-02-29 11:15:51 +0600

    BestSeller Data

---
+++ BestSellerData/BestsellerData.js
@@ -0,0 +1,45 @@
+import product5 from "../src/assets/product5.png";
+import product6 from "../src/assets/product6.png";
+import product7 from "../src/assets/product7.png";
+import product8 from "../src/assets/product8.png";
+
+const BestSellerData = [
+  {
+    id: 1,
+    title: "Basic Crew Neck Tee",
+    price: "$50.00",
+    badge: true,
+    color: true,
+    productImg: product5,
+    productStatus: "New",
+  },
+  {
+    id: 2,
+    title: "Basic Crew Neck Tee",
+    price: "$50.00",
+    badge: true,
+    color: true,
+    productImg: product6,
+    productStatus: "Trending",
+  },
+  {
+    id: 3,
+    title: "Basic Crew Neck Tee",
+    price: "$50.00",
+    badge: true,
+    color: true,
+    productImg: product7,
+    productStatus: "Sold",
+  },
+  {
+    id: 4,
+    title: "Basic Crew Neck Tee",
+    price: "$50.00",
+    badge: true,
+    color: false,
+    productImg: product8,
+    productStatus: "75%",
+  },
+];
+
+export default BestSellerData;
