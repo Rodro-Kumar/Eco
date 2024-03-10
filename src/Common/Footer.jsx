94da2abe1d84a40856b2cdf8780a0da3e9e3fddf
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-10 19:29:00 +0600

    footer modify

--- src/Common/Footer.jsx
+++ src/Common/Footer.jsx
@@ -1,116 +1,83 @@
 import React from "react";
-import Flex from "../Common/Flex";
-import Logo from "../assets/logo.png";
-import { FaFacebookF } from "react-icons/fa";
-import { FaLinkedinIn } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
+import FooterItem from "./FooterItem";
+import logo from "../assets/logo.png";
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <>
-      <div className="bg-[#F5F5F3] footer">
+      <div className="py-14">
         <div className="container">
-          <div className="py-14">
-            <Flex className={" gap-x-36"}>
-              <ul>
-                <li>MENU</li>
-                <li>
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <a href="#">Shop</a>
-                </li>
-                <li>
-                  <a href="#">About</a>
-                </li>
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-                <li>
-                  <a href="#">Journal</a>
-                </li>
-              </ul>
-              <ul>
-                <li>SHOP</li>
-                <li>
-                  <a href="#">Category 1</a>
-                </li>
-                <li>
-                  <a href="#">Category 2</a>
-                </li>
-                <li>
-                  <a href="#">Category 3</a>
-                </li>
-                <li>
-                  <a href="#">Category 4</a>
-                </li>
-                <li>
-                  <a href="#">Category 5</a>
-                </li>
-              </ul>
-              <ul>
-                <li>HELP</li>
-                <li>
-                  <a href="#">Privacy Policy</a>
-                </li>
-                <li>
-                  <a href="#">Terms & Conditions</a>
-                </li>
-                <li>
-                  <a href="#">Special E-shop</a>
-                </li>
-                <li>
-                  <a href="#">Shipping</a>
-                </li>
-                <li>
-                  <a href="#">Secure Payments</a>
-                </li>
-              </ul>
+          <div className="flex justify-between">
+            <FooterItem
+              title={"MENU"}
+              allitem={["Home", "Shop", "About", "Contact", "Journal"]}
+            />
+            <FooterItem
+              title={"SHOP"}
+              allitem={[
+                "Category",
+                "Category",
+                "Category",
+                "Category",
+                "Category",
+              ]}
+            />
+            <FooterItem
+              title={"HELP"}
+              allitem={[
+                "Privacy Policy",
+                "Terms & Conditions",
+                "Special E-shop",
+                "Shipping",
+                "Secure Payments",
+              ]}
+            />
 
-              <div>
-                <p className="text-base font-DMsans font-bold text-primaryFontColor">
-                  (052) 611-5711
-                </p>
-                <p className="text-base font-DMsans font-bold text-primaryFontColor">
-                  company@domain.com
-                </p>
-                <p className="text-sm font-DMsans font-normal text-thirdFontColor pt-3">
-                  575 Crescent Ave. Quakertown, PA 18951
-                </p>
+            <div>
+              <div className="text-[#262626] font-DMsans font-bold text-lg">
+                <a href="tel:(052) 611-5711">(052) 611-5711</a>
               </div>
-              <div>
-                <picture>
-                  <img src={Logo} alt={Logo} />
-                </picture>
+              <div className="text-[#262626] font-DMsans font-bold text-lg pb-3">
+                <a href="mailto:company@domain.com">company@domain.com</a>
               </div>
-            </Flex>
-            <Flex className="pt-14 items-center justify-between">
-              <Flex className={"gap-x-5 icon"}>
-                <a
-                  href="#"
-                  className="text-2xl  items-center justify-center flex transition-all h-10 w-10  rounded-full"
-                >
-                  <FaFacebookF className="" />
-                </a>
-                <a
-                  href="#"
-                  className="text-2xl items-center justify-center flex transition-all h-10 w-10  rounded-full transition-all"
-                >
-                  <FaLinkedinIn />
-                </a>
-                <a
-                  href="#"
-                  className="text-2xl items-center justify-center flex transition-all h-10 w-10  rounded-full transition-all"
-                >
-                  <FaInstagram />
-                </a>
-              </Flex>
-              <div>
-                <p className="text-sm font-DMsans font-normal text-thirdFontColor">
-                  2020 Orebi Minimal eCommerce Figma Template by Adveits
-                </p>
+
+              <div className="text-[#6D6D6D] font-DMsans font-normal text-sm">
+                <p>575 Crescent Ave. Quakertown, PA 18951</p>
               </div>
-            </Flex>
+            </div>
+            <div>
+              <picture className="cursor-pointer">
+                <img src={logo} alt={logo} />
+              </picture>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-x-4 pt-16">
+              <a
+                href="#"
+                className="h-10 w-10 rounded-full flex justify-center items-center hover:bg-[#316FF6] hover:text-white hover:-translate-y-1 transition-all"
+              >
+                <FaFacebookF className="text-xl" />
+              </a>
+              <a
+                href="#"
+                className="h-10 w-10 rounded-full flex justify-center items-center hover:bg-[#0077b5] hover:text-white hover:-translate-y-1 transition-all"
+              >
+                <FaLinkedinIn className="text-xl" />
+              </a>
+              <a
+                href="#"
+                className="h-10 w-10 rounded-full flex justify-center items-center hover:bg-[#bb3088] hover:text-white hover:-translate-y-1 transition-all"
+              >
+                <FaInstagram className="text-xl" />
+              </a>
+            </div>
+            <div>
+              <p className="text-[#6D6D6D] font-DMsans font-normal text-sm">
+                2020 Orebi Minimal eCommerce Figma Template by Adveits
+              </p>
+            </div>
           </div>
         </div>
       </div>
