89ff04c115863514d1aba109dc3de2ba3d1db89b
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-24 19:53:53 +0600

    button update

--- src/Common/Button.jsx
+++ src/Common/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ className, children }) => {
   return (
     <button
-      className={` bg-primaryFontColor text-primaryBgColor text-sm font-DMsans font-bold py-4 px-14 ${className}`}
+      className={` bg-primaryFontColor text-black text-sm font-DMsans font-bold py-4 px-14 ${className}`}
     >
       {children ? children : "Button"}
     </button>
