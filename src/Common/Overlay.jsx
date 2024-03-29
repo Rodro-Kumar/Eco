1e5dd4a90d2ca870558e8cbb11aff934878b9861
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-29 09:54:03 +0600

    modified

--- src/Common/Overlay.jsx
+++ src/Common/Overlay.jsx
@@ -11,17 +11,21 @@ const Overlay = ({
   return (
     <>
       <div className={className}>
-        <h3 className="text-4xl font-DMsans font-bold text-primaryFontColor pb-9">
+        <h3 className="text-3xl sm:text-4xl font-DMsans font-bold text-primaryFontColor pb-7">
           {AdsHeading ? AdsHeading : "Phones Sale "}
         </h3>
-        <p className="text-base font-DMsans font-normal text-[#262626c4] pb-12">
+        <p className="text-sm sm:text-base font-DMsans font-normal sm:text-[#262626c4] text-yellow-400 pb-10 whitespace-nowrap sm:whitespace-normal">
           Up to
-          <span className="text-[40px] font-bold text-primaryFontColor px-2 align-middle">
+          <span className="text-[35px] text-yellow-400 sm:text-black sm:text-[40px] font-bold text-primaryFontColor px-2 align-middle">
             {Discout ? Discout : "30%"}
           </span>
-          {DiscooutRightText ? DiscooutRightText : "sale for all phones!"}
+          <span className="text-[#262626c4]">
+            {DiscooutRightText ? DiscooutRightText : "sale for all phones!"}
+          </span>
         </p>
-        <Button>{btnTitle ? btnTitle : "Shop Now"}</Button>
+        <Button className={"py-[10px] px-8 sm:py-4 sm:px-14"}>
+          {btnTitle ? btnTitle : "Shop Now"}
+        </Button>
       </div>
     </>
   );
