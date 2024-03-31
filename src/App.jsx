ecfe688dd42a85bd2b1bd0b44d3aecdfdab405a7
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-03-31 10:47:07 +0600

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Home from "./Pages/Home/Home";
+import RootLayout from "./Common/RootLayout/RootLayout";
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<RootLayout />} path="/">
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <>
+    <RouterProvider router={router}>
       <Home />
-    </>
+    </RouterProvider>
   );
 }
 
