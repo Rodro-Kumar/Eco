1d438f6bdad8d2bb6b7d49f40530fc5a8d1e9256
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-05 12:14:09 +0600

    modified

--- src/Common/Card.jsx
+++ src/Common/Card.jsx
@@ -10,25 +10,29 @@ const Card = ({ colorVariant, badge, img, productTitle, price }) => {
         <div className="group relative overflow-hidden">
           <div className="absolute top-5 left-5 text-black">{badge}</div>
 
-          <img src={img ? img : productImg} alt={img} />
+          <img
+            src={img ? img : productImg}
+            alt={img}
+            className="sm:w-full sm:h-[220px] md:h-auto"
+          />
 
           {/* =================Overlay================= */}
 
-          <div className="py-6 px-7 flex flex-col items-end absolute -bottom-36 left-0 bg-white w-full gap-y-5 group-hover:bottom-0 transition-all">
+          <div className="py-6 px-7 flex flex-col items-center md:items-end absolute -bottom-36 left-0 bg-white w-full gap-y-5 group-hover:bottom-0 transition-all">
             <div className="flex items-center gap-x-4">
-              <h5 className="text-[#767676] font-DMsans hover:text-[#262626] hover:font-bold transition-all">
+              <h5 className="text-[#767676] font-DMsans hover:text-[#262626] hover:font-bold transition-all sm:text-sm md:text-base">
                 Add to List
               </h5>
               <FaHeart />
             </div>
-            <div className="flex items-center gap-x-4">
-              <h5 className="text-[#767676] font-normal hover:text-[#262626] hover:font-bold transition-all">
+            <div className="flex items-center gap-x-8 md:gap-x-6">
+              <h5 className="text-[#767676] font-normal hover:text-[#262626] hover:font-bold transition-all sm:text-sm md:text-base">
                 Compare
               </h5>
               <TbReload />
             </div>
             <div className="flex items-center gap-x-4">
-              <h5 className="text-[#767676] font-normal hover:text-[#262626] hover:font-bold transition-all">
+              <h5 className="text-[#767676] font-normal hover:text-[#262626] hover:font-bold transition-all sm:text-sm md:text-base">
                 Add to Cart
               </h5>
               <FaShoppingCart />
@@ -38,16 +42,16 @@ const Card = ({ colorVariant, badge, img, productTitle, price }) => {
           {/* =================Overlay================= */}
         </div>
 
-        <div className="flex justify-between items-center pt-6">
-          <h3 className="text-primaryFontColor font-DMsans font-semibold text-xl">
+        <div className="flex justify-between items-center pt-6 sm:pt-3 md:pt-6 sm:flex-col sm:items-start lg:flex-row lg:items-center">
+          <h3 className="text-primaryFontColor font-DMsans font-semibold text-xl sm:text-base md:text-xl">
             {productTitle ? productTitle : "Basic Crew Neck Tee"}
           </h3>
-          <p className="text-base font-DMsans font-normal text-thirdFontColor">
+          <p className="text-base font-DMsans font-normal text-thirdFontColor sm:text-sm md:text-base">
             {price ? price : "$44.00"}
           </p>
         </div>
         {colorVariant && (
-          <span className="text-thirdFontColor font-DMsans font-normal text-base pt-3">
+          <span className="text-thirdFontColor font-DMsans font-normal text-base pt-3 sm:text-sm md:text-base">
             Black
           </span>
         )}
