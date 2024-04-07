797f1c0d03d5fa7df7167a450806e2a33240b8c2
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-07 10:00:24 +0600

    modified

--- src/Home_Component/Ads2/Ads2.jsx
+++ src/Home_Component/Ads2/Ads2.jsx
@@ -7,19 +7,25 @@ const Ads2 = () => {
     <>
       <div className="pb-32">
         <div className="container">
-          <div className="bg-[#f3f3f3] flex items-center sm:py-5 sm:pr-5 md:py-0 md:px-0">
+          <div className="bg-[#f3f3f3] flex items-center py-3 sm:py-5 sm:pr-5 md:py-0 md:px-0">
             <div>
-              <img src={clockimg} alt={clockimg} />
+              <img
+                src={clockimg}
+                alt={clockimg}
+                className="h-[15vh] sm:h-auto"
+              />
             </div>
             <div>
-              <h2 className="text-4xl font-DMsans font-bold text-primaryFontColor pb-9 sm:text-3xl sm:pb-4 md:text-4xl md:pb-9">
+              <h2 className="text-xl font-DMsans font-bold text-primaryFontColor pb-3 sm:text-3xl sm:pb-4 md:text-4xl md:pb-9">
                 Phone of the year
               </h2>
-              <p className="text-base sm:text-sm md:text-base font-DMsans font-normal text-primaryFontColor pb-12 max-w-[510px] sm:pb-6 md:pb-12">
+              <p className="text-xs sm:text-sm md:text-base font-DMsans font-normal text-primaryFontColor pb-4 max-w-[510px] sm:pb-6 md:pb-12">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry orem Ipsum..
               </p>
-              <Button>Shop Now</Button>
+              <Button className={"py-[6px] px-[16px] lg:py-4 lg:px-14"}>
+                Shop Now
+              </Button>
             </div>
           </div>
         </div>
