9055297b181682f69262318e82eac86655f8e7e2
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-07 09:58:55 +0600

    modified

--- src/Common/Button.jsx
+++ src/Common/Button.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Button = ({ className, children }) => {
   return (
-    <button
-      className={` bg-primaryFontColor text-white text-sm font-DMsans font-bold py-4 px-14 hover:bg-[#353535] transition-all hover:scale-95 ${className}`}
+    <Button
+      className={`bg-primaryFontColor text-white text-sm font-DMsans font-bold py-4 px-14 hover:bg-[#353535] transition-all hover:scale-95 ${className}`}
     >
       {children ? children : "Button"}
-    </button>
+    </Button>
   );
 };
 
