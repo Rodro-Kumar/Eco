4a118092c4c3afc6d9e15b3f765698445be1b29f
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-04-07 10:02:27 +0600

    modified

--- src/Home_Component/SpecialOffer/SpecialOffer.jsx
+++ src/Home_Component/SpecialOffer/SpecialOffer.jsx
@@ -84,8 +84,8 @@ const settings = {
     {
       breakpoint: 770,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToShow: 3,
+        slidesToScroll: 3,
       },
     },
     {
@@ -94,6 +94,13 @@ const settings = {
         slidesToShow: 2,
         slidesToScroll: 2,
         initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 414,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
     },
     {
